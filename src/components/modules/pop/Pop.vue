497db8f3b29ad082ef8a772b68ac9a021e09c92c
a1597284782 <1597284782@qq.com>
2020-09-03 17:22:22 +0800

    完成签到组件

---
+++ src/components/modules/pop/Pop.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="tips animation" v-show="isShow" :class="{'shake': type === 'shake'}" ref="tips">
+    <div class="content">{{msg}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pop',
+  props: {
+    // 类型
+    type: {
+      type: String,
+      default: ''
+    },
+    // 提示信息
+    msg: {
+      type: String,
+      default: ''
+    },
+    // 显示隐藏
+    isShow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    isShow (newval, oldval) {
+      if (newval !== oldval && newval === true) {
+        setTimeout(() => {
+          const height = this.$refs.tips.clientHeight
+          const width = this.$refs.tips.clientWidth
+          this.$refs.tips.style.marginLeft = -width / 2 + 'px'
+          this.$refs.tips.style.marginTop = -height / 2 + 'px'
+        }, 0)
+        setTimeout(() => {
+          this.isShow = false
+          this.msg = ''
+          this.type = ''
+        }, 3000)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes shake {
+  0%,
+  100% {
+    transform: translateX(0);
+  }
+
+  10%,
+  30%,
+  50%,
+  70%,
+  90% {
+    transform: translateX(-10%);
+  }
+  20%,
+  40%,
+  60%,
+  80% {
+    transform: translateX(10%);
+  }
+}
+
+.tips {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.animation {
+  animation-fill-mode: both;
+  animation-duration: 0.3s;
+}
+
+.content {
+  background: rgba($color: #000000, $alpha: 0.4);
+  color: #fff;
+  border-radius: 6px;
+  padding: 10px 15px;
+}
+
+.shake {
+  animation-name: shake;
+}
+</style>
--- src/components/sidebar/Sign.vue
+++ src/components/sidebar/Sign.vue
@@ -11,21 +11,26 @@
       </a>
       <span class="fly-signin-days">
         已连续签到
-        <cite>16</cite>天
+        <cite>{{count}}</cite>天
       </span>
     </div>
     <div class="fly-panel-main fly-signin-main">
-      <button class="layui-btn layui-btn-danger" id="LAY_signin">今日签到</button>
-      <span>
-        可获得
-        <cite>5</cite>飞吻
-      </span>
+      <template v-if="!isSign">
+        <button class="layui-btn layui-btn-danger" id="LAY_signin" @click="sign()">今日签到</button>
+        <span>
+          可获得
+          <cite>{{favs}}</cite>飞吻
+        </span>
+      </template>
 
       <!-- 已签到状态 -->
-      <!--
-          <button class="layui-btn layui-btn-disabled">今日已签到</button>
-          <span>获得了<cite>20</cite>飞吻</span>
-      -->
+      <template v-else>
+        <button class="layui-btn layui-btn-disabled">今日已签到</button>
+        <span>
+          获得了
+          <cite>{{favs}}</cite>飞吻
+        </span>
+      </template>
     </div>
     <!-- 签到说明 -->
     <sign-info :isShow="isShow" @closeModal="close()"></sign-info>
@@ -37,6 +42,8 @@
 <script>
 import SignInfo from './SignInfo'
 import SignList from './SignList'
+import moment from 'dayjs'
+import { userSign } from '@/api/user'
 
 export default {
   name: 'sign',
@@ -51,7 +58,59 @@ export default {
       // 控制 活跃榜 显示隐藏
       showList: false,
       // 活跃榜选项卡 切换值
-      current: 0
+      current: 0,
+      // 控制签到按钮 是否可点击
+      isSign: false,
+      // 是否已登录
+      isLogin: this.$store.state.isLogin
+    }
+  },
+  mounted () {
+    // 判断用户上一次的签到时间与签到状态
+    // 如果用户上一次签到时间与当天时间相差1天，则允许用户签到
+    const isSign = this.$store.state.userInfo.isSign
+    const lastSign = this.$store.state.userInfo.lastSign
+    const nowDate = moment().format('YYYY-MM-DD')
+    const lastDate = moment(lastSign).format('YYYY-MM-DD')
+    const diff = moment(nowDate).diff(moment(lastDate), 'day')
+    if (diff > 0 && isSign) {
+      this.isSign = false
+    } else {
+      this.isSign = isSign
+    }
+  },
+  computed: {
+    // 获得的积分数
+    favs () {
+      const count = parseInt(this.count)
+      let result = 0
+      if (count < 5) {
+        result = 5
+      } else if (count >= 5 && count < 15) {
+        result = 10
+      } else if (count >= 15 && count < 30) {
+        result = 15
+      } else if (count >= 30 && count < 100) {
+        result = 20
+      } else if (count >= 100 && count < 365) {
+        result = 30
+      } else if (count >= 365) {
+        result = 50
+      }
+
+      return result
+    },
+    // 用户签到了多少天
+    count () {
+      if (this.$store.state.userInfo !== {}) {
+        if (typeof this.$store.state.userInfo.count !== 'undefined') {
+          return this.$store.state.userInfo.count
+        } else {
+          return 0
+        }
+      } else {
+        return 0
+      }
     }
   },
   methods: {
@@ -71,6 +130,28 @@ export default {
     // 活跃榜选项卡切换
     choose (val) {
       this.current = val
+    },
+    // 点击签到
+    sign () {
+      if (!this.isLogin) {
+        this.$pop('shake', '请先登录')
+        return
+      }
+      userSign().then(res => {
+        const user = this.$store.state.userInfo
+        if (res.code === 200) {
+          user.favs = res.favs
+          user.count = res.count
+          this.$pop('', '签到成功')
+        } else {
+          // 用户已经签到了
+          this.$pop('', '您已经签到过了')
+        }
+        user.isSign = true
+        user.lastSign = res.lastSign
+        this.isSign = true
+        this.$store.commit('setUserInfo', user)
+      })
     }
   }
 }
