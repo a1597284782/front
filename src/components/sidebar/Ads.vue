a39c03d2d264af7bd27dcf9095c68f69504001b4
Brian <1597284782@qq.com>
2021-05-09 14:57:34 +0800

    调整广告区和友链

--- src/components/sidebar/Ads.vue
+++ src/components/sidebar/Ads.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="fly-panel">
-    <div class="fly-panel-title">这里可作为广告区域</div>
-    <div class="fly-panel-main">
+    <!-- <div class="fly-panel-title">广告区域</div> -->
+    <!-- <div class="fly-panel-main">
       <a
         target="_blank"
         class="fly-zanzhu"
         time-limit="2017.09.25-2099.01.01"
         style="background-color: #5FB878;"
       >LayIM 3.0 - layui 旗舰之作</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
